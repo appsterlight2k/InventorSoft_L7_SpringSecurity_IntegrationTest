7546532f25cb40adce1c42124d6454bdf12bfccc
appsterlight <appsterlight@gmail.com>
2023-11-01 00:50:39 +0200

    Set default constructor of Error constants class to private

--- src/main/java/co/inventorsoft/academy/spring/restfull/controller/constants/Error.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/controller/constants/Error.java
@@ -1,8 +1,10 @@
 package co.inventorsoft.academy.spring.restfull.controller.constants;
 
 public final class Error {
+    private Error() { }
+
     public static final String DISABLED_USER_MESSAGE = "Disabled user %s tried to authenticate!";
-    public static final String BAD_CREDENTIALS_MESSAGE = "User %s was entered bad credentials!";
+    public static final String BAD_CREDENTIALS_MESSAGE = "User %s entered incorrect credentials!";
     public static final String USERNAME_NOT_FOUND_MESSAGE = "Username %s not found!";
     public static final String INSUFFICIENT_AUTH_MESSAGE = "Insufficient authentication for user %s!";
 
